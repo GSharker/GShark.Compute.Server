8a9dc090f460c691e034b558e634fcc2b4bf0aff
Cesare Caoduro <Cesare.Caoduro@aecom.com>
2021-10-28 10:07:24 +1100

    moved folders

--- src/Gshark.Compute.API/Controllers/WeatherForecastController.cs
+++ src/Gshark.Compute.API/Controllers/WeatherForecastController.cs
@@ -1,6 +1,11 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace GShark.Compute.Server.Api.Controllers
+namespace Gshark.Compute.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -8,8 +13,8 @@ namespace GShark.Compute.Server.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -21,11 +26,12 @@ namespace GShark.Compute.Server.Api.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
         }
--- src/Gshark.Compute.API/WeatherForecast.cs
+++ src/Gshark.Compute.API/WeatherForecast.cs
@@ -1,4 +1,6 @@
-namespace GShark.Compute.Server.Api
+using System;
+
+namespace Gshark.Compute.API
 {
     public class WeatherForecast
     {
@@ -8,6 +10,6 @@ namespace GShark.Compute.Server.Api
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
